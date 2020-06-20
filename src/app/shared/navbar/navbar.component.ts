8f5e83536d2f048c7623363d59e69db6ee506cbf
Edward Navarro <edward@unicauca.edu.co>
2020-06-20 15:21:35 -0500

    diseño e implementacion de la barra de navegacion

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  title: string
+  items: string[]
+
+  constructor() {
+    this.title = 'Edward Navarro'
+    this.items = ['Perfil Profesional', 'portafolio', 'contacto']
+  }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
