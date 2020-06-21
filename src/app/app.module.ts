9e64eb888e7d9e624d06287cd9231e1b7f8442ab
Edward Navarro <edward@unicauca.edu.co>
2020-06-21 10:11:05 -0500

    implementacion del routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { PortfolioComponentComponent } from './components/portfolio-component/portfolio-component.component';
+import { ContactComponentComponent } from './components/contact-component/contact-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponentComponent,
+    PortfolioComponentComponent,
+    ContactComponentComponent
   ],
   imports: [
     BrowserModule,
